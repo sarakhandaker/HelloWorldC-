2c4daa80823bb826f449fd0fd0742b5d11e4de51
sarakhandaker <kttsara@gmail.com>
2020-09-14 17:57:42 -0700

    properties

--- HelloWorld/Person.cs
+++ HelloWorld/Person.cs
@@ -5,21 +5,35 @@ namespace HelloWorld
     {
         public string FirstName;
         public string LastName;
-        private DateTime _birthdate;
+        //private DateTime _birthdate;
+        public DateTime Birthdate {get; private set;}
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
+                return years;
+            }
+        }
 
         public void Introduce()
         {
             Console.WriteLine(FirstName + " " + LastName);
         }
 
-        public void SetBirthdate(DateTime birthdate)
-        {
-            _birthdate = birthdate;
-        }
+        //public void SetBirthdate(DateTime birthdate)
+        //{
+        //    _birthdate = birthdate;
+        //}
 
-        public DateTime GetBirthdate()
-        {
-            return _birthdate;
-        }
+        //public DateTime GetBirthdate()
+        //{
+        //    return _birthdate;
+        //}
     }
 }
