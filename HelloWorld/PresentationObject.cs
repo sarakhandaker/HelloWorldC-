f1269b0358cd1f7352f5d4aa69fd3fedb5f74098
sarakhandaker <kttsara@gmail.com>
2020-09-15 11:23:08 -0700

    inheritence

---
+++ HelloWorld/PresentationObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Presentation
+    {
+        public Presentation()
+        {
+        }
+    }
+}
---
+++ HelloWorld/Text.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Text
+    {
+        public Text()
+        {
+        }
+    }
+}
