491167f7d1da3d9e3b6dd0de222e527affacf1a8
sarakhandaker <kttsara@gmail.com>
2020-09-16 14:32:46 -0700

    my stack class

--- HelloWorld/Car.cs
+++ HelloWorld/Car.cs
@@ -3,7 +3,7 @@ namespace HelloWorld
 {
     public class Car : Vehicle
     {
-        public Car(string reg) :base(res)
+        public Car(string reg) :base(reg)
         {
             Console.WriteLine("Car is being initialized");
         }
---
+++ HelloWorld/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    public class Stack
+    {
+        public List<object> ClassStack{ get; private set; }
+
+        public Stack()
+        {
+            this.ClassStack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj==null)
+            {
+                throw new InvalidOperationException("Object is Null");
+            }
+
+            this.ClassStack.Add(obj);
+
+        }
+        public object Pop()
+        {
+            if (this.ClassStack.Count==0)
+            {
+                
+            }
+            var removed = this.ClassStack[ClassStack.Count - 1];
+            this.ClassStack.RemoveAt(ClassStack.Count - 1);
+            return removed;
+        }
+
+        public void Clear()
+        {
+            this.ClassStack = new List<object>();
+        }
+    }
+}
