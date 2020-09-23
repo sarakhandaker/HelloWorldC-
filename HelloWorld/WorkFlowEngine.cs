ed29a72b5571716d9ea2b731e2325eb56078b1ef
sarakhandaker <kttsara@gmail.com>
2020-09-23 13:27:05 -0700

    work flow exercise

---
+++ HelloWorld/WorkFlowEngine.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    public interface IActivity
+    {
+        void Execute();
+    }
+
+    public interface IWorkFlow
+    {
+        void AddActivity(IActivity activity);
+        void RemoveActivity(IActivity activity);
+        IEnumerable<IActivity> GetTasks();
+    }
+
+    public class WorkFlow : IWorkFlow
+    {
+        private List<IActivity> _workFlowList { get;}
+
+        public WorkFlow()
+        {
+            this._workFlowList = new List<IActivity>();
+        }
+
+        public void AddActivity(IActivity activity)
+        {
+            _workFlowList.Add(activity);
+        }
+
+        public void RemoveActivity(IActivity activity)
+        {
+            _workFlowList.Remove(activity);
+        }
+
+        public IEnumerable<IActivity> GetTasks()
+        {
+            return _workFlowList;
+        }
+    }
+
+    public class VideoUploader : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Video is Uploading");
+        }
+    }
+
+    public class SendEmail : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Email is Sending");
+        }
+    }
+
+    public class WorkFlowEngine
+    {
+        public void Run(IWorkFlow workFlow)
+        {
+            foreach (IActivity activity in workFlow.GetTasks())
+                {
+                activity.Execute();
+            }
+
+        }
+    }
+}
