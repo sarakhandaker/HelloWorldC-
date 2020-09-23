b6d22b8b21cca4b41742ef8b8620a9b61df40d9a
sarakhandaker <kttsara@gmail.com>
2020-09-22 17:00:05 -0700

    polymorphism exercises

---
+++ HelloWorld/DbCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class DbCommand
+    { 
+        public DbConnection Connection { get; set; }
+        public string Instruction { get; }
+
+        public DbCommand(DbConnection connect, string instruction)
+        {
+            if (connect == null || instruction==null || instruction=="")
+                throw new InvalidOperationException("Connection is Null");
+            this.Connection = connect;
+            this.Instruction = instruction;
+        }
+
+        public void Execute()
+        {
+            this.Connection.OpeningDb();
+            Console.WriteLine(this.Instruction); 
+            this.Connection.ClosingDb();
+        }
+    }
+}
---
+++ HelloWorld/DbConnection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connect) : base(connect)
+            {
+            }
+
+        public override void ClosingDb()
+        {
+            Console.WriteLine("Closing SQL");
+        }
+
+        public override void OpeningDb()
+        {
+            Console.WriteLine("Opening SQL");
+        }
+    }
+
+    public class OracleConnection: DbConnection
+    {
+        public OracleConnection(string connect) : base(connect)
+        {
+        }
+
+        public override void ClosingDb()
+        {
+            Console.WriteLine("Closing Oracle");
+        }
+
+        public override void OpeningDb()
+        {
+            Console.WriteLine("Opening Oracle");
+        }
+    }
+
+    public abstract class DbConnection
+    {
+        public string ConnectionString { get; set; }
+        public TimeSpan Timeout { get; set; }
+
+        public DbConnection(string connection)
+        {
+            if (connection==null || connection== "")
+                throw new InvalidOperationException("ConnectionString is Null");
+            this.ConnectionString = connection;
+        }
+
+        public abstract void OpeningDb();
+
+        public abstract void ClosingDb();
+    }
+}
